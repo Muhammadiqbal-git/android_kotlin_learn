a8b9826b5c7bbe6660bf457eb79a5fcd59560655
iqbal-gk-mac <iqbal.dev.code@gmail.com>
2024-12-31 13:07:58 +0700

    done splash screen

---
+++ app/src/main/java/com/learn/android/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.learn.android
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
